1f7639df6dc88534e028b6b5a62e0b3675e745d7
Eman Radwan <emanalaa11499@gmail.com>
2025-12-07 14:36:49 +0200

    setup next-themes to support dark mode

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { ThemeProvider } from "next-themes";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 const Layout: React.FC = () => {
   return (
-    <>
+    <ThemeProvider>
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
